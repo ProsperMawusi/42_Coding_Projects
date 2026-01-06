f621f86c776cdb546ac71d8564ad9841d87f3622
Prosper Mawusi <pmawusi@c1r10s2.42wolfsburg.de>
2026-01-06 12:45:17 +0100

    Yet to be tested if program works

--- ft_getnextline/getnextline.c
+++ ft_getnextline/getnextline.c
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getnextline.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmawusi <pmawusi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 10:54:41 by pmawusi           #+#    #+#             */
+/*   Updated: 2026/01/06 12:26:07 by pmawusi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./get_next_line.h"
 #include <fcntl.h>
 
 int	main ()
 {
 	int		fd;
-	char	*line; 
+	char	*line;
 
 	fd = open ("tests/simple", O_RDONLY);
 	while (1)
 	{
 		line = get_next_line(fd);
 		if (line == NULL)
-			break;
+			break ;
 		printf("%s", line);
 		freee(line);
 	}
